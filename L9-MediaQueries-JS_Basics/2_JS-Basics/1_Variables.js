9090ebe46a7cea369e46bb84d916040fa990a6e0
Kartik <kartik.mathur10@gmail.com>
2025-02-17 21:56:31 +0530

    JS Basics

---
+++ L9-MediaQueries-JS_Basics/2_JS-Basics/1_Variables.js
@@ -0,0 +1,17 @@
+var a = 10
+console.log(a);
+
+a = "hello world!"
+console.log(a)
+
+for (var i = 1; i <= 4; i++) {
+    console.log(i)
+}
+
+i = 1;
+while (i <= 3) {
+
+    console.log(i);
+
+    i++;
+}
