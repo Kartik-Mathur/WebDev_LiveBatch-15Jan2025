e0465142b78ce348434e78d10c212903a02e29a5
Kartik <kartik.mathur10@gmail.com>
2025-03-10 22:57:46 +0530

    Callbacks

--- L12-AsyncProgramming/4_ExecutingAsyncTasksSequentially/1_Callbacks/3_Task-Callback.js
+++ L12-AsyncProgramming/4_ExecutingAsyncTasksSequentially/1_Callbacks/3_Task-Callback.js
@@ -1,23 +1,37 @@
-function download(url){
-    let profileImg = url.split('/').pop();
+function download(url, cb) {
+    console.log("Download starts at url:", url);
     // Maan lena profileImg nikaalne mei 2 seconds lagte hai
-    console.log(profileImg);
+    setTimeout(() => {
+        let profileImg = url.split('/').pop();
+        console.log("Download ends", profileImg);
+
+        cb(profileImg, upload); // cb is actually compress
+    }, 2000);
 }
 
-function compress(profileImg){
-    // jpg se hume webp format mei convert karna hai
-    compressedImg = profileImg.split('.')[0] + '.webp';
-    // Maan lena profileImg ko compress krne mei 2 seconds lagte hai
-    console.log(compressedImg)
+function compress(profileImg, cb) {
+    console.log("compress starts of", profileImg);
+
+    setTimeout(() => {
+        let compressedImg = profileImg.split('.')[0] + '.webp';
+        console.log("compression completed", compressedImg);
+        cb(compressedImg); // upload function is callback of compress
+    }, 2000);
+    // jpg se hume webp format mei convert karna hai  
 }
 
-function upload(compressedImg){
-    let newUrl = 'http://meraNayaWebsite.com/'+compressedImg
-    // Maan lena compressedImg ko new url par upload krne mei 2 seconds lagte hai
-    console.log(newUrl);
+function upload(compressedImg) {
+    console.log("Upload starts of:", compressedImg);
+
+    setTimeout(() => {
+        let newUrl = 'http://meraNayaWebsite.com/' + compressedImg
+        // Maan lena compressedImg ko new url par upload krne mei 2 seconds lagte hai
+        console.log("NEW URL:", newUrl);
+    }, 2000);
 }
 
-// download('http://devanshkisite.ug/image/devansh.jpg')
+download('http://devanshkisite.ug/image/devansh.jpg', compress);
+
 // compress('devansh.jpg');
 // upload('devansh.web');
 
