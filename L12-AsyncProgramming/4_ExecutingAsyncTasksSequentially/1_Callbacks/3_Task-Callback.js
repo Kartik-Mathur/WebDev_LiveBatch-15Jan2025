041573e8022017c809d98909aa551056f06bb3cc
Kartik <kartik.mathur10@gmail.com>
2025-03-10 22:22:24 +0530

    Callback-Task

---
+++ L12-AsyncProgramming/4_ExecutingAsyncTasksSequentially/1_Callbacks/3_Task-Callback.js
@@ -0,0 +1,21 @@
+function download(url){
+    let profileImg = url.split('/').pop();
+    // Maan lena profileImg nikaalne mei 2 seconds lagte hai
+    console.log(profileImg);
+}
+
+function compress(profileImg){
+    // jpg se hume webp format mei convert karna hai
+    compressedImg = profileImg.split('.')[0] + '.webp';
+    // Maan lena profileImg ko compress krne mei 2 seconds lagte hai
+    console.log(compressedImg)
+}
+
+function upload(compressedImg){
+    let newUrl = 'http://meraNayaWebsite.com/'+compressedImg
+    // Maan lena compressedImg ko new url par upload krne mei 2 seconds lagte hai
+    console.log(newUrl);
+}
+
+// download('http://devanshkisite.ug/image/devansh.jpg')
+// download -> compress -> upload
