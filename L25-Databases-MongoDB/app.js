e8705148c006302acdfec5d4e563f4bbc2a531be
Kartik <kartik.mathur10@gmail.com>
2025-04-14 21:39:36 +0530

    MongoDB

--- L25-Databases-MongoDB/app.js
+++ L25-Databases-MongoDB/app.js
@@ -12,14 +12,60 @@ const url = 'mongodb://localhost:27017';
 const client = new MongoClient(url);
 
 // Database Name
-const dbName = 'Todos'; // Agar DB exist karta hai toh select krega, else create krega new DB
+const dbName = 'Tasks'; // Agar DB exist karta hai toh select krega, else create krega new DB
 
+let db = null;
+
+client.connect()
+    .then(() => {
+        console.log("nodejs connect with mongodb done")
+        db = client.db(dbName);
+    })
+    .catch(err => {
+        throw new Error(err.message); // Server ko crash kr dega
+    })
+
+// Collection ka naam should be 'Todos' -> To store Documents
 app.get('/todos', (req, res) => {
     // returns all the available todos
+    let Todos = db.collection('Todos'); // Agar Todos naam ka collection hai toh use krega, else create krega
+
+    Todos.find().toArray()
+        .then(data => {
+            res.send({
+                msg: 'Todos fetched success',
+                tasks: data
+            })
+        })
+        .catch(err => {
+            res.send({
+                msg: err.message
+            })
+        })
 })
 
 app.post('/todos', (req, res) => {
+    const { task } = req.body;
     // Add a new todo to the mongodb database
+    let Todos = db.collection('Todos'); // Agar Todos naam ka collection hai toh use krega, else create krega
+
+    Todos.insertOne({
+        task,
+        status: false
+    })
+
+    res.send({
+        message: "Insertion done",
+        task
+    });
+})
+
+app.put('/todos', (req, res) => {
+
+})
+
+app.delete('/todos', (req, res) => {
+
 })
 
 
