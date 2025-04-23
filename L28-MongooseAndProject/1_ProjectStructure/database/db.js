8ef9c9a97e7edf05400cdaf5a4b19a27e60c4732
Kartik <kartik.mathur10@gmail.com>
2025-04-23 21:42:26 +0530

    Advanced mongoose

--- L28-MongooseAndProject/1_ProjectStructure/database/db.js
+++ L28-MongooseAndProject/1_ProjectStructure/database/db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 async function connectMongo() {
+    let url = `mongodb+srv://kartik:kartik@cluster0.97kax2o.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
     await mongoose.connect('mongodb://localhost:27017/todos');
 }
 
