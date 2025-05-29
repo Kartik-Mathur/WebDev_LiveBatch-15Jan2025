d90592dff6b9ad66cae834105e8dae810167345d
Kartik <kartik.mathur10@gmail.com>
2025-05-29 22:06:33 +0530

    Postgresql

--- L32-Postgresql/src/crud.ts
+++ L32-Postgresql/src/crud.ts
@@ -13,35 +13,60 @@ export async function createUser(name: string, email: string): Promise<User> {
 }
 
 export async function getUserById(id: number): Promise<User | null> {
- const { rows } = await pool.query(
-   `SELECT * FROM users WHERE id = $1`,
+  const { rows } = await pool.query(`SELECT * FROM users WHERE id = $1`, [id]);
+  return rows[0] ?? null;
+}
+
+export async function getAllUsers(): Promise<User[]> {
+  const { rows } = await pool.query(`SELECT * FROM users`);
+  return rows;
+}
+
+export async function updateUser(
+  id: number,
+  name: string,
+  email: string
+): Promise<User | null> {
+  const { rows } = await pool.query<User>(
+    `UPDATE users
+    SET name = $1, email = $2
+    WHERE id = $3
+    RETURNING *`,
+    [name, email, id]
+  );
+  return rows[0] ?? null;
+}
+
+export async function deleteUser(id: number): Promise<User | null> {
+ const { rows } = await pool.query<User>(
+   `DELETE FROM users WHERE id = $1 RETURNING *`,
    [id]
  );
  return rows[0] ?? null;
 }
 
-export async function getAllUsers(): Promise<User[]> {
- const { rows } = await pool.query(
-   `SELECT * FROM users`
- );
- return rows;
-}
 
+(async function () {
+  console.log("Creating User");
+  const user = await createUser("Kartik Mathur","kartik3@gmail.com");
+  console.log(user);
 
-(async function (){
-    // console.log("Creating User");
-    // const user = await createUser("Kartik Mathur","kartik3@gmail.com");
-    // console.log(user);
+  // console.log("Reading user with id : 3");
+  // const user = await getUserById(3);
+  // console.log(user);
 
-    console.log("Reading user with id : 3");
-    const user = await getUserById(3);
-    console.log(user);
+  // console.log("Reading all users");
+  // const users = await getAllUsers();
+  // for (const item of users) {
+  //   console.log(item);
+  // }
 
-    console.log("Reading all users");
-    const users = await getAllUsers();
-    for (const item of users) {
-        console.log(item);
-    }
-    
-    
-})()
+  // console.log("Updating User: ");
+  // const updatedData = await updateUser(3, "Aashish", "ashish@gmail.com");
+  // console.log(updatedData);
+
+  // console.log("Deleting User: ");
+  // const deletedUser = await deleteUser(3);
+  // console.log(deletedUser);
+
+})();
