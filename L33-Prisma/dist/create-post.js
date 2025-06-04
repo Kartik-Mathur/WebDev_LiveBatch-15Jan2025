e4c317c648ffcf09f006969d87ad4756fa6921b6
Kartik <kartik.mathur10@gmail.com>
2025-06-04 21:53:18 +0530

    CSR and SSR-HBS

---
+++ L33-Prisma/dist/create-post.js
@@ -0,0 +1,61 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+// 1
+const prisma_1 = require("../generated/prisma");
+const extension_accelerate_1 = require("@prisma/extension-accelerate");
+// 2
+const prisma = new prisma_1.PrismaClient().$extends((0, extension_accelerate_1.withAccelerate)());
+// 3
+function main() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const newUser = yield prisma.user.create({
+            data: {
+                email: 'Ashish@swiggy.com',
+                name: 'Ashish Gupta',
+                posts: {
+                    create: {
+                        title: 'Majnu bhai painting',
+                        content: 'Ghodha Ghadhe ki madad krta hua'
+                    }
+                }
+            },
+        });
+        console.log("Created user:", newUser);
+    });
+}
+function insertPostInExistingUser() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const newPost = yield prisma.post.create({
+            data: {
+                title: 'Monalisa Painting',
+                content: 'Amazing Painting Replica',
+                author: {
+                    connect: {
+                        email: 'Prabhsimran@atlassian.com'
+                    }
+                }
+            },
+        });
+        console.log("Updated user:", newPost);
+    });
+}
+// 4
+insertPostInExistingUser()
+    .then(() => __awaiter(void 0, void 0, void 0, function* () {
+    yield prisma.$disconnect();
+}))
+    .catch((e) => __awaiter(void 0, void 0, void 0, function* () {
+    console.error(e);
+    // 5
+    yield prisma.$disconnect();
+    process.exit(1);
+}));
