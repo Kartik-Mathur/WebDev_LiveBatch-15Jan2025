e7ee675352f60d8ff24908226f19cd16e135cebc
Kartik <kartik.mathur10@gmail.com>
2025-06-16 20:33:29 +0530

    React-Router Introduction

--- L37-ReactJs/1_rerendering/src/main.jsx
+++ L37-ReactJs/1_rerendering/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode, useState } from "react";
 import { createRoot } from "react-dom/client";
-import RenderFix1 from "./RenderFix1";
+import RenderFix2 from "./RenderFix2";
 
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RenderFix1 />
+    <RenderFix2 />
   </StrictMode>
 );
